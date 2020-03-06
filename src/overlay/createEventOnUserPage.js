5ff1b01fb92396c7101085b029f0cbd97a650ecd
alexbit <bityukov.a@concord-consulting.org>
2020-03-06 18:01:19 +0300

    Added previous values

--- src/overlay/createEventOnUserPage.js
+++ src/overlay/createEventOnUserPage.js
@@ -1,10 +1,21 @@
-const sendIntervalDescription = () => {
-    const event = document.createEvent('Event');
-    event.initEvent('SendIntervalInfo');
-    document.dispatchEvent(event);
-}
+const sendEvent = (eventStr) => () => {
+  const event = document.createEvent('Event');
+  event.initEvent(eventStr);
+  document.dispatchEvent(event);  
+};
 
-document.getElementById("intervalInfoButton").addEventListener("click", sendIntervalDescription);
+const sendIntervalDescription = sendEvent('sendIntervalInfo');
+const sendIntervalFromPrev = sendEvent('sendIntervalFromPrev');
+const sendIntervalYoutube = sendEvent('sendIntervalYoutube');
+const sendIntervalTwitch = sendEvent('sendIntervalTwitch');
+const sendIntervalHabr = sendEvent('sendIntervalHabr');
+const sendIntervalBudget = sendEvent('sendIntervalBudget');
+const sendIntervalPriory1 = sendEvent('sendIntervalPriory1');
+const sendIntervalPriory2 = sendEvent('sendIntervalPriory2');
+const sendIntervalPriory3 = sendEvent('sendIntervalPriory3');
+const sendIntervalPriory4 = sendEvent('sendIntervalPriory4');
+const sendIntervalPriory5 = sendEvent('sendIntervalPriory5');
+
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
@@ -12,10 +23,14 @@ document.addEventListener("keyup", function(event) {
     }
 });
 
-const sendIntervalFromPrev = () => {
-  const event = document.createEvent('Event');
-  event.initEvent('sendIntervalFromPrev');
-  document.dispatchEvent(event);
-}
-
+document.getElementById("intervalInfoButton").addEventListener("click", sendIntervalDescription);
 document.getElementsByClassName('wrapPrevIntervalDescription')[0].addEventListener("click", sendIntervalFromPrev);
+document.getElementsByClassName('youtubeInterval')[0].addEventListener("click", sendIntervalYoutube);
+document.getElementsByClassName('twitchInterval')[0].addEventListener("click", sendIntervalTwitch);
+document.getElementsByClassName('habrInterval')[0].addEventListener("click", sendIntervalHabr);
+document.getElementsByClassName('budgetInterval')[0].addEventListener("click", sendIntervalBudget);
+document.getElementsByClassName('priority1Interval')[0].addEventListener("click", sendIntervalPriory1);
+document.getElementsByClassName('priority2Interval')[0].addEventListener("click", sendIntervalPriory2);
+document.getElementsByClassName('priority3Interval')[0].addEventListener("click", sendIntervalPriory3);
+document.getElementsByClassName('priority4Interval')[0].addEventListener("click", sendIntervalPriory4);
+document.getElementsByClassName('priority5Interval')[0].addEventListener("click", sendIntervalPriory5);
--- src/overlay/index.js
+++ src/overlay/index.js
@@ -16,17 +16,82 @@
     });
   });
 
+  const getPriorityIntervals = async () => {
+    const intervals = await new Promise((resolve) => {
+      chrome.storage.local.get('intervalLog', ({ intervalLog = [] }) => {
+        resolve(intervalLog);
+      });
+    });
+
+    const priorityIntervals = intervals.reduce((acc, {description}) => {
+      const unicId = acc.findIndex(({description: unicDescription}) => unicDescription === description);
+      if(unicId !== -1)
+        acc[unicId].count++;
+      else 
+        acc.push({
+          count: 1,
+          description,
+        }); 
+        
+      return acc; 
+    }, []);
+
+    priorityIntervals.sort(({count: firstCount}, {count: secondCount}) => secondCount - firstCount);
+
+    return priorityIntervals.slice(0, 5);
+  }
+  const setLinkStyle = (elem) => {
+    const setDefaultStyle = (elem) => {
+      elem.style.color = '#1a0dab';
+      elem.style.cursor = 'default';
+      elem.style.textDecoration = 'none';
+    };
+
+    setDefaultStyle(elem);
+
+    elem.onmouseover = ({ target }) => {
+      target.style.color = '#1a0dab';
+      target.style.cursor = 'pointer';
+      target.style.textDecoration = 'underline';
+    };
+    
+    elem.onmouseout = ({ target }) => {
+      setDefaultStyle(target);
+    };
+  };
+
+  const listClassNames = [
+    'priority1Interval',
+    'priority2Interval',
+    'priority3Interval',
+    'priority4Interval',
+    'priority5Interval'
+  ];
+
   if(document.getElementById('intervalInfo') === null) {
     const overlay = document.createElement('div');
     overlay.innerHTML = `
-      <div>
+      <div class="root">
         <div class="setNewInterval">
-          <input type="text" id="intervalInfo" />
-          <button id="intervalInfoButton" >Send</button>
+          <input type="text" id="intervalInfo" class="buttonTimeManagment" />
+          <button id="intervalInfoButton" class="buttonTimeManagment">Send</button>
         </div>
         <div class="wrapPrevIntervalDescription">
           <a class="prevIntervalDescription">${await getLastIntervalDescription()}</a>
         </div>
+        <div class="quickDescInterval">
+          <button class="youtubeInterval buttonTimeManagment">YouTube</button>
+          <button class="twitchInterval buttonTimeManagment">Twitch</button>
+          <button class="habrInterval buttonTimeManagment">Habr</button>
+          <button class="budgetInterval buttonTimeManagment">Бюджет</button>
+        </div>
+        <ul class="wrapPrevIntervalsDescription">
+          <li class="priority1Interval"></li>
+          <li class="priority2Interval"></li>
+          <li class="priority3Interval"></li>
+          <li class="priority4Interval"></li>
+          <li class="priority5Interval"></li>
+        </ul>
       </div>`;
     overlay.id = 'timeManagmenOverlay';
     overlay.style.position = 'fixed';
@@ -41,34 +106,67 @@
     overlay.style.background = 'white';
     document.body.append(overlay);
     const prevIntervalElemWrap = document.getElementsByClassName('wrapPrevIntervalDescription')[0];
-    prevIntervalElemWrap.style.paddingTop = '10px';
-    prevIntervalElemWrap.style.textAlign = 'center';
+    prevIntervalElemWrap.style.padding = '10px 0 10px 10px';
+    prevIntervalElemWrap.style.textAlign = 'left';
     const prevIntervalElem = document.getElementsByClassName('prevIntervalDescription')[0];
+    const quickDescIntervalElem = document.getElementsByClassName('quickDescInterval')[0];
+    quickDescIntervalElem.style.display = 'flex';
+    quickDescIntervalElem.style.justifyContent = 'space-between';
+    const newIntervalElem = document.getElementsByClassName('setNewInterval')[0];
+    newIntervalElem.style.display = 'flex';
+    newIntervalElem.style.justifyContent = 'center';
+    const rootElem = document.getElementsByClassName('root')[0];
+    rootElem.style.width = '300px';
+    const intervalInfoElem = document.getElementById('intervalInfo');
+    intervalInfoElem.style.width = '100%';
+    setLinkStyle(prevIntervalElem);
+    const buttons = document.getElementsByClassName('buttonTimeManagment');
+    for (let elem of buttons) {
+      elem.style.fontSize = '14px';
+      elem.style.fontWeight = '400';
+      elem.style.padding = '1px 7px 2px';
+      elem.style.borderWidth = '1px';
+      elem.style.borderStyle = 'solid';
+      elem.style.borderColor = 'gb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186)';
+      elem.style.borderImage = 'initial';
+      elem.style.borderRadius = '0';
+      elem.style.color = 'rgba(0, 0, 0, 0.847)';
+      elem.style.background = 'rgb(255, 255, 255)';
+      elem.style.lineHeight = '21px';
 
-    const setDefaultStyle = (elem) => {
-      elem.style.color = '#1a0dab';
-      elem.style.cursor = 'default';
-      elem.style.textDecoration = 'none';
+      elem.onmouseover = ({ target }) => {
+        target.style.cursor = 'pointer';
+      };
+      
+      elem.onmouseout = ({ target }) => {
+        elem.style.cursor = 'default';
+      };
     }
-    
-    setDefaultStyle(prevIntervalElem);
-
-    prevIntervalElem.onmouseover = ({ target }) => {
-      target.style.color = '#1a0dab';
-      target.style.cursor = 'pointer';
-      target.style.textDecoration = 'underline';
-    };
-    
-    prevIntervalElem.onmouseout = ({ target }) => {
-      setDefaultStyle(target);
-    };
+    listClassNames.forEach((value) => {
+      document.getElementsByClassName(value)[0].style.display = 'none';
+    });
   }
 
   const showOverlay = async () => {
     document.getElementById('timeManagmenOverlay').style.display = 'flex';
     document.getElementById('intervalInfo').focus();
     document.getElementsByClassName('prevIntervalDescription')[0].textContent = await getLastIntervalDescription();
-  }
+    const priorityIntervals = await getPriorityIntervals();
+    const wrapPrevIntervalsDescriptionElem = document.getElementsByClassName('wrapPrevIntervalsDescription')[0];
+    wrapPrevIntervalsDescriptionElem.style.padding = '10px 0 0 15px';
+    wrapPrevIntervalsDescriptionElem.style.listStyle = 'disc outside';
+    listClassNames.forEach((listElem) => {
+      const intervalElem = document.getElementsByClassName(listElem)[0];
+      intervalElem.style.display = 'none';
+    });
+    priorityIntervals.forEach(({description}, key) => {
+      const intervalElem = document.getElementsByClassName(listClassNames[key])[0];
+      intervalElem.textContent = description;
+      setLinkStyle(intervalElem);
+      intervalElem.style.paddingTop = '5px';
+      intervalElem.style.display = 'list-item';
+    });
+  };
   
   const hideOverlay = () => document.getElementById('timeManagmenOverlay').style.display = 'none';
 
@@ -78,20 +176,34 @@
 
   (document.head || document.documentElement).appendChild(script);
 
-  document.addEventListener("SendIntervalInfo", () => {
-    const intervalInfo = document.getElementById('intervalInfo');
-    if(intervalInfo.value) {
-      hideOverlay();
-      chrome.runtime.sendMessage(createMessage(SEND_INTERVAL_INFO, intervalInfo.value), Function.prototype);
-    }
-    intervalInfo.value = '';
-  });
+  const addEventListenerSendInterval = (value, eventStr) => {
+    document.addEventListener(eventStr, async () => {
+      let nexValue = value;
+      const intervalInfo = document.getElementById('intervalInfo');
 
-  document.addEventListener("sendIntervalFromPrev", async () => {
-    const lastIntervalDescription = await getLastIntervalDescription();
-    chrome.runtime.sendMessage(createMessage(SEND_INTERVAL_INFO, lastIntervalDescription), Function.prototype);
-    hideOverlay();
-  });
+      if(typeof value === 'function') {
+        nexValue = await value();
+      }
+      
+      if(nexValue) {
+        hideOverlay();
+        chrome.runtime.sendMessage(createMessage(SEND_INTERVAL_INFO, nexValue), Function.prototype);
+      }
+      intervalInfo.value = '';
+    });
+  };
+
+  addEventListenerSendInterval(() => document.getElementById('intervalInfo').value, 'sendIntervalInfo'); 
+  addEventListenerSendInterval(async () => await getLastIntervalDescription(), 'sendIntervalFromPrev'); 
+  addEventListenerSendInterval('Смотрю Youtube', 'sendIntervalYoutube'); 
+  addEventListenerSendInterval('Смотрю Twitch', 'sendIntervalTwitch'); 
+  addEventListenerSendInterval('Читаю Habr', 'sendIntervalHabr'); 
+  addEventListenerSendInterval('Считаю бюджет', 'sendIntervalBudget'); 
+  addEventListenerSendInterval(async () => (await getPriorityIntervals())[0].description, 'sendIntervalPriory1'); 
+  addEventListenerSendInterval(async () => (await getPriorityIntervals())[1].description, 'sendIntervalPriory2'); 
+  addEventListenerSendInterval(async () => (await getPriorityIntervals())[2].description, 'sendIntervalPriory3'); 
+  addEventListenerSendInterval(async () => (await getPriorityIntervals())[3].description, 'sendIntervalPriory4'); 
+  addEventListenerSendInterval(async () => (await getPriorityIntervals())[4].description, 'sendIntervalPriory5'); 
 
   chrome.runtime.onMessage.addListener(
     (request = {}, sender, sendResponse) => {
